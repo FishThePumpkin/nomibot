f8a38d441af1b38bf6a0dac696bc9e50740e982a
FishThePumpkin <39113773+FishThePumpkin@users.noreply.github.com>
2018-05-21 13:52:52 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 
 
 // Set the prefix
-const prefix = "!";
+const prefix = "n!";
 client.on("message", (message) => {
   // Exit and stop if it's not there
   if (!message.content.startsWith(prefix)) return;
